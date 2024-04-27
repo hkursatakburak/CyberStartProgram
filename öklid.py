f0be4342dff7643a66fbf573a3164fb25f2a4e60
hkursatakburak <akburakkursat54@gmail.com>
2024-04-27 13:11:23 +0300

    homework finished

--- öklid.py
+++ öklid.py
@@ -1,1 +1,33 @@
-print("başlıyoruz")
+"""Göreviniz:
+
+Python'da fonksiyonlar ve döngüler kavramlarını kullanarak, aşağıdaki işlemleri gerçekleştiren bir program yazmanız gerekmektedir:
+
+Noktaların Tanımlanması:
+
+‘points’ adında bir liste oluşturun. Bu liste, 2D uzaydaki noktaları temsil eden demetler (tuple) içermelidir. 
+Örneğin, ‘(x, y)’ noktası bir demet ‘(x, y)’ olarak temsil edilecektir.
+
+Öklid Mesafesi İçin Bir Fonksiyon Yazma:
+
+‘euclideanDistance’ adında bir fonksiyon tanımlayın. Bu fonksiyon, iki demet (her biri bir noktayı temsil eder) almalı ve bu iki nokta arasındaki Öklid mesafesini döndürmelidir.
+
+Mesafelerin Hesaplanması:
+
+Bir döngü kullanarak, ‘points’ listesindeki her nokta çifti arasındaki Öklid mesafesini hesaplayın. Bu mesafeleri ‘distances’ adında başka bir listede saklayın.
+
+Minimum Mesafenin Bulunması:
+
+‘distances’ listesinden minimum mesafeyi bulun ve yazdırın."""
+
+
+points = [(1, 2), (4, 6), (8, 9)]
+def euclideanDistance(point1, point2):
+    return ((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2) ** 0.5
+
+distances = []
+for i in range(len(points)):
+    for j in range(i+1, len(points)):
+        distances.append(euclideanDistance(points[i], points[j]))
+
+min_distance = min(distances)
+print("Minimum mesafe ", min_distance)
