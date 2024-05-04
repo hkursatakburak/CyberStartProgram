4edc85f21cd4f7690d486ce5e7d92ee43b14a2d7
hkursatakburak <akburakkursat54@gmail.com>
2024-05-04 19:28:19 +0300

    fixed

--- öklid.py
+++ öklid.py
@@ -20,14 +20,13 @@ Minimum Mesafenin Bulunması:
 ‘distances’ listesinden minimum mesafeyi bulun ve yazdırın."""
 
 
-points = [(1, 2), (4, 6), (8, 9)]
+points = [(1, 2), (4, 6), (8, 9), (9,5)]
 def euclideanDistance(point1, point2):
-    return ((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2) ** 0.5
+    return pow(pow(point1[0] - point2[0], 2) + pow(point1[1] - point2[1],2), 0.5),
 
 distances = []
-for i in range(len(points)):
+for i in range(len(points) -1):
     for j in range(i+1, len(points)):
         distances.append(euclideanDistance(points[i], points[j]))
 
-min_distance = min(distances)
-print("Minimum mesafe ", min_distance)
+print(min(distances))
